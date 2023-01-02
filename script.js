68d133728fe90f13412d35e665661337e8105e29
MasonMarc <masonmarcantel@gmail.com>
2023-01-02 16:30:32 -0500

    got api to work with class and gen html

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const config = require('./config');
+const Album = require('./lib/Album');
 
 
 
@@ -11,7 +12,15 @@ var getResult = function (artistName, albumName) {
         .then(function (response) {
             if (response.ok) {
                 response.json().then(function (data) {
-                    console.log(data);
+                    console.log(data.album.artist + ' hey');
+                    console.log(data.album.name);
+                    console.log(data.album.tags.tag[0].name);
+                    const album = new Album(data.album.name, data.album.artist, data.album.tags.tag[0].name)
+                    fs.appendFile('./dist/album.html', album.albumHtml(data.album.name, data.album.artist, data.album.tags.tag[0].name), function (error) {
+                        if (error) {
+                            throw error;
+                        }
+                    });
                 });
             } else {
                 alert('Error: ' + response.statusText);
@@ -37,10 +46,36 @@ const choosealbum = () => {
             name: 'albumName',
         }
     ])
-    .then((answers) => {
-        getResult(answers.artistName, answers.albumName)
-    }
+        .then((answers) => {
+            getResult(answers.artistName, answers.albumName)
+            addMore();
+        }
 
-)};
+        )
+};
+
+
+const addMore = () => {
+    const prompt = inquirer.createPromptModule();
+    prompt([
+        {
+            type: 'list',
+            message: 'Would you like to add another album?',
+            choices: ['yes', 'no'],
+            name: 'select'
+        }])
+        .then((answer) => {
+            if (answer.select === 'yes') {
+                choosealbum();
+            }
+            else if (answer.select === 'no') {
+                fs.appendFile('./dist/album.html', `<body><html>`, function (error) {
+                    if (error) throw error;
+                })
+            }
+
+        })
+}
+
 
 choosealbum();
